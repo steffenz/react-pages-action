3eb9c38a2dd922dd93d4d9727b6b0e64ec7e477f
Publishbot <publishbot@publishbot.com>
2020-04-02 14:15:07 +0000

    publish new version

---
+++ precache-manifest.c260fa4a3591f406096bfd46289b52d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f5de0cfec5b8deac12d762bc23f009",
+    "url": "/react-pages-action/index.html"
+  },
+  {
+    "revision": "0e6e68b589f9addfd45d",
+    "url": "/react-pages-action/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1124f57cad0113d87ecd",
+    "url": "/react-pages-action/static/js/2.dde0c515.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-pages-action/static/js/2.dde0c515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6e68b589f9addfd45d",
+    "url": "/react-pages-action/static/js/main.c0f2db59.chunk.js"
+  },
+  {
+    "revision": "c70c750ae4877fd06b97",
+    "url": "/react-pages-action/static/js/runtime-main.5a9b03d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages-action/static/media/logo.5d5d9eef.svg"
+  }
+]);
